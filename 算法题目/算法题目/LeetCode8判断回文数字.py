a047165d12625c6dac0df6e7a3a13441a6821230
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-02-25 17:36:58 +0800

    Create LeetCode8判断回文数字.py

---
+++ 算法题目/算法题目/LeetCode8判断回文数字.py
@@ -0,0 +1,23 @@
+#Author guo
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        '''
+        p=str(x)
+
+        tmp=p[::-1]
+        if p==tmp:
+            return True
+        else:
+            return False
+        '''
+
+        p = x
+        res = 0
+        while p:
+            res = res * 10 + p % 10
+            p = p // 10
+
+        return res == x
