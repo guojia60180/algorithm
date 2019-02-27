173123d17643ceaa9db49d6b1c903f4b44dbec31
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-02-27 17:37:39 +0800

    Create LeetCode27去除元素返回个数.py

---
+++ 算法题目/算法题目/LeetCode27去除元素返回个数.py
@@ -0,0 +1,17 @@
+#Author guo
+
+class Solution:
+    def removeElement(self, nums, val):
+        low, high = 0, len(nums) - 1
+        while 1:
+            while low < len(nums) and nums[low] != val:
+                low += 1
+
+            while high >= 0 and nums[high] == val:
+                high -= 1
+
+            if low >= high:
+                break
+
+            nums[low], nums[high] = nums[high], nums[low]
+        return low
