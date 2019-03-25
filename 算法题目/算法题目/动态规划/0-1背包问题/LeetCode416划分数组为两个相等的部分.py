bb6bf2736926caa74eb3946d80ef1d5c53b77a61
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-03-25 14:40:14 +0800

    Create LeetCode416划分数组为两个相等的部分.py

---
+++ 算法题目/算法题目/动态规划/0-1背包问题/LeetCode416划分数组为两个相等的部分.py
@@ -0,0 +1,19 @@
+#Author guo
+class Solution:
+    def canPartition(self, nums):
+        s=sum(nums)
+        if s%2!=0:
+            return False
+
+
+        dp=[0]*(s+1)
+        dp[0]=1
+        for num in nums:
+            for i in range(s,-1,-1):
+                if dp[i]:
+                    dp[i+num]=1
+
+            if dp[s//2]:
+                return True
+
+        return False
