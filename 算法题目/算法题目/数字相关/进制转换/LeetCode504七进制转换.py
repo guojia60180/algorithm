b260dee1628c795838bdb3a9b9dec729a6a9073f
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-04-08 15:36:09 +0800

    Create LeetCode504七进制转换.py

---
+++ 算法题目/算法题目/数字相关/进制转换/LeetCode504七进制转换.py
@@ -0,0 +1,13 @@
+#Author guo
+class Solution:
+    def convertToBase7(self, num):
+        if num==0:
+            return '0'
+        res=[]
+        sign=num>=0
+        num=abs(num)
+        while num!=0:
+            res.append(num%7)
+            num//=7
+
+        return (''if sign else '-')+''.join(map(str,res[::-1]))
