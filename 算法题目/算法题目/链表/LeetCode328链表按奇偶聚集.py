0013ca1e5747b45dc483ea8df1e463b70de715d5
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-04-10 16:07:22 +0800

    Create LeetCode328链表按奇偶聚集.py

---
+++ 算法题目/算法题目/链表/LeetCode328链表按奇偶聚集.py
@@ -0,0 +1,23 @@
+#Author guo
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def oddEvenList(self, head):
+        if head==None:
+            return head
+        odd=head
+        even=head.next
+        evenhead=even
+
+        while even !=None and even.next!=None:
+            odd.next=odd.next.next
+            odd=odd.next
+            even.next=even.next.next
+            even=even.next
+
+        odd.next=evenhead
+        return head
