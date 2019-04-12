8a5ac5f884a81ccde1d37ca43d4918f072efb519
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-04-12 20:53:31 +0800

    Create LeetCode104二叉树最大深度.py

---
+++ 算法题目/算法题目/树/LeetCode104二叉树最大深度.py
@@ -0,0 +1,13 @@
+#Author guo
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        if root==None:
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
