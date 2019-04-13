b0c6bbaaf8013938f040d1ef430348f2ca9d5538
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-04-13 10:39:10 +0800

    Update LeetCode108有序数组中构造二叉查找树.py

--- 算法题目/算法题目/树/BST二叉查找树/LeetCode108有序数组中构造二叉查找树.py
+++ 算法题目/算法题目/树/BST二叉查找树/LeetCode108有序数组中构造二叉查找树.py
@@ -1,20 +1,21 @@
 #Author guo
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
 
 class Solution:
     def sortedArrayToBST(self, nums):
-        return self.toBST(nums,0,len(nums)-1)
+        return self.toBST(nums, 0, len(nums) - 1)
 
-    def toBST(self,nums,sidx,eidx):
-        if sidx>eidx:
+    def toBST(self, nums, sidx, eidx):
+        if sidx > eidx:
             return None
-        midx=(sidx+eidx)//2
-        root=nums[midx]
-        root.left=self.toBST(nums,sidx,midx-1)
-        root.right=self.toBST(nums,midx+1,eidx)
+        midx = (sidx + eidx) // 2
+        root = TreeNode(nums[midx])
+        # root=nums[midx]
+        root.left = self.toBST(nums, sidx, midx - 1)
+        root.right = self.toBST(nums, midx + 1, eidx)
         return root
