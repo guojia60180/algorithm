369915a3e6e5d78095d66f44abd7522169c48be6
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-05-08 09:41:56 +0800

    Create 题25归并两个排序链表.py

---
+++ 算法题目/算法题目/剑指offer/题25归并两个排序链表.py
@@ -0,0 +1,26 @@
+#Author guo
+# -*- coding:utf-8 -*-
+class ListNode:
+     def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution:
+    # 返回合并后列表
+    def Merge(self, pHead1, pHead2):
+        # write code here
+        if pHead1==None:
+            return pHead2
+        if pHead2==None:
+            return pHead1
+
+        pHead=None
+
+        if(pHead1.val<pHead2.val):
+            pHead=pHead1
+            pHead.next=self.Merge(pHead1.next,pHead2)
+
+        if(pHead1.val>=pHead2.val):
+            pHead=pHead2
+            pHead.next=self.Merge(pHead1,pHead2.next)
+
+        return pHead
